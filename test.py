a57beab552496efcec896ccaf0a1c53e6b63123e
Aviv <avivfriedman100@gmail.com>
2024-10-24 10:50:30 +0300

    Remove certificates from tracking, update .gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-CERT_DIR = os.path.join(os.getcwd(), 'certificates')
-SERVER_CERT_PATH = os.path.join(CERT_DIR, 'server_cert.pem')
-SERVER_KEY_PATH = os.path.join(CERT_DIR, 'server_key.pem')
-CLIENT_CERT_PATH = os.path.join(CERT_DIR, 'client_cert.pem')
-CLIENT_KEY_PATH = os.path.join(CERT_DIR, 'client_key.pem')
-CA_CERT_PATH = os.path.join(CERT_DIR, 'ca_cert.pem')
